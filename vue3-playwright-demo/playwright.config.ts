f7a18aa13a81c8f762d341e238bc5284bdc19803
mooncat126 <xiangyuforwork@gmail.com>
2025-02-10 22:14:04 +0900

    chore: typescript setting

---
+++ vue3-playwright-demo/playwright.config.ts
@@ -0,0 +1,11 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  // 指定测试目录
+  testDir: './tests',  
+  use: {
+    // 浏览器配置，如头部模式等
+  }
+};
+
+export default config;
